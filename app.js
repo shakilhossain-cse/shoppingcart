d7e38e21ac30d3477a0d9a92b1fb3834e08e34c2
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-08-18 15:34:34 +0600

    complate cart contity and quantity price section

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 const plusbtn = document.getElementById('incrisebtn');
 const minusbtn = document.getElementById('decrisebtn');
+const phnplusbtn = document.getElementById('phnincrise');
+const phnminusbtn = document.getElementById('phndecrise');
 const inputValue = document.getElementById('input');
+const inputValuePhn = document.getElementById('inputphn');
 let price = document.getElementById('price');
+let phnPrice = document.getElementById('phnPrice');
 
 
-function quantitySum(isPlus) {
-    const value = parseInt(inputValue.value);
+function quantitySum(isPlus,money,input,amount) {
+    const value = parseInt(input.value);
     if (isPlus) {
         const number = value + 1;
-        inputValue.value = number;
-        price.innerText = number * 59;
+        input.value = number;
+        amount.innerText = number * money;
     }else{
         const number = value - 1;
         if (number > 0) {
-            inputValue.value = number;
-            price.innerText = number * 59;
+            input.value = number;
+            amount.innerText = number * money;
         }
     }
 }
 
+
+
+phnplusbtn.addEventListener('click',function () {
+    quantitySum(true,1219,inputValuePhn,phnPrice)
+});
+
+phnminusbtn.addEventListener('click',function () {
+    quantitySum(false,1219,inputValuePhn,phnPrice)
+});
+
+
 plusbtn.addEventListener('click',function () {
-    quantitySum(true)
+    quantitySum(true,59,inputValue,price)
 });
 
 minusbtn.addEventListener('click',function () {
-    quantitySum(false)
+    quantitySum(false,59,inputValue,price)
    
 });
