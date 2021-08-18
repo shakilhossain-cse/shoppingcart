f314140e519d7b5be5c9603b0d8f9f5f734ed848
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-08-18 14:44:41 +0600

    complate one cart price functon

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('I am in a separate file')
+const plusbtn = document.getElementById('incrisebtn');
+const minusbtn = document.getElementById('decrisebtn');
+const inputValue = document.getElementById('input');
+let price = document.getElementById('price');
+
+
+function quantitySum(isPlus) {
+    const value = parseInt(inputValue.value);
+    if (isPlus) {
+        const number = value + 1;
+        inputValue.value = number;
+        price.innerText = number * 59;
+    }else{
+        const number = value - 1;
+        if (number > 0) {
+            inputValue.value = number;
+            price.innerText = number * 59;
+        }
+    }
+}
+
+plusbtn.addEventListener('click',function () {
+    quantitySum(true)
+});
+
+minusbtn.addEventListener('click',function () {
+    quantitySum(false)
+   
+});
