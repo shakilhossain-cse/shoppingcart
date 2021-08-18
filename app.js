5551a48e0db65aa6ee1c8697e1f438d2ac0471aa
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-08-18 17:45:40 +0600

    done sopping cart program

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
-const plusbtn = document.getElementById('incrisebtn');
-const minusbtn = document.getElementById('decrisebtn');
-const phnplusbtn = document.getElementById('phnincrise');
-const phnminusbtn = document.getElementById('phndecrise');
-const inputValue = document.getElementById('input');
-const inputValuePhn = document.getElementById('inputphn');
-let price = document.getElementById('price');
-let phnPrice = document.getElementById('phnPrice');
-
-
-function quantitySum(isPlus,money,input,amount) {
-    const value = parseInt(input.value);
-    if (isPlus) {
-        const number = value + 1;
-        input.value = number;
-        amount.innerText = number * money;
-    }else{
-        const number = value - 1;
-        if (number > 0) {
-            input.value = number;
-            amount.innerText = number * money;
-        }
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
     }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
 }
 
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
 
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // update on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
 
-phnplusbtn.addEventListener('click',function () {
-    quantitySum(true,1219,inputValuePhn,phnPrice)
+// phone increase decrease events
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
 });
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+})
 
-phnminusbtn.addEventListener('click',function () {
-    quantitySum(false,1219,inputValuePhn,phnPrice)
+
+// handle case increase decrease events
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
 });
-
-
-plusbtn.addEventListener('click',function () {
-    quantitySum(true,59,inputValue,price)
-});
-
-minusbtn.addEventListener('click',function () {
-    quantitySum(false,59,inputValue,price)
-   
-});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+})
